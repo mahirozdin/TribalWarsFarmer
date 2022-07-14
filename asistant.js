6bcfa9ef78ff8639855a222f1e5fc9084f5b5974
Mahir Taha Özdin <ozdinmt@gmail.com>
2022-07-15 01:39:02 +0300

    1.0.0

--- asistant.js
+++ asistant.js
@@ -1,21 +1,4 @@
 
-// ==UserScript==
-// @name          TW Farm Assistant Plus
-// @namespace     https://github.com/sindresorhus/cssprettifier-userscript
-// @version       0.9.1
-// @description   One click farm helper for Tribal Wars.
-// @author        TheVTM
-// @require       https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.10.1/lodash.js
-// @match         *://*.*/game.php?*screen=am_farm*
-// @grant         GM_getValue
-// @grant         GM_setValue
-// @grant         GM_addStyle
-// @grant         none
-// ==/UserScript==
-
-
-
-
 
 var manager = "
 
